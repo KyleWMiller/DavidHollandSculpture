ea4b3fc124c9a192b609b54d96881ba9adea9f22
architMahto <architmahto@cmail.carleton.ca>
2016-06-02 23:10:58 -0600

    Completed functionality for comments.

--- public/controllers/commentsController.js
+++ public/controllers/commentsController.js
@@ -7,12 +7,16 @@
   function commentsController($firebaseArray) {
     var commentsCtrl = this;
 
-    var ref = new Firebase('https://sweltering-fire-9565.firebaseio.com/comments');
+    // var ref = new Firebase('https://sweltering-fire-9565.firebaseio.com/comments');
 
     commentsCtrl.comment = {};
+    commentsCtrl.comments = [{name: "Kyle Miller",
+                              content: "I felt the complexity of this image based off of something that I felt/I am an art major and I know what I am talking about. This piece invoked this emotion and I am now a complete humanbeing. ...."},
+                             {name: "Archit Mahto",
+                              content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ad nesciunt commodi, magni adipisci architecto explicabo sit porro, odio dicta ipsa mollitia veniam in necessitatibus consequatur, quod assumenda fugiat, sapiente?"}];
 
     commentsCtrl.addComment = function() {
-      console.log(commentsCtrl.comment);
+      commentsCtrl.comments.push(commentsCtrl.comment);
       commentsCtrl.comment = {};
     }
   }
