524bb3496020db64f216877e7117448bfea2e24c
aremmer <miller.kwill@gmail.com>
2016-06-11 18:17:20 -0600

    adding table of contents

--- public/controllers/mailController.js
+++ public/controllers/mailController.js
@@ -12,11 +12,10 @@
     mailCtrl.email = {};
 
     mailCtrl.sendEmail = function() {
-      console.log(mailCtrl.email);
 
       emailFactory.send(mailCtrl.email)
         .then(function(response) {
-          console.log(response);
+          console.log(response)
           mailCtrl.email = {};
           // console.log("Successfully sent email.");
         })
